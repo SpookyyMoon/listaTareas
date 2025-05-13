f908eef90636f7054ccad11fd8656342c0ab4800
SpookyyMoon <spookyluna666@gmail.com>
2025-05-13 17:18:44 +0200

    Fix creación de tareas
    -Ahora las tareas se crean con un ID
    -Las tareas ya no se clonan y se generar con una función que las crea de forma independiente

--- script/script.js
+++ script/script.js
@@ -1,31 +1,36 @@
 $(document).ready(function () {
     var contadorTareas = 0;
-    const tarea = $(`<div class=tarea>
-                          <div class="contenedorTarea">
-                            <p class="tituloTarea" id="tarea${contadorTareas}">
-                            Título de la tarea:
-                                <input>
-                                </input>
-                            </p>
-                            <p class="descripciónTarea">
-                            Descripción de la tarea:
-                                <textarea>
-                                </textarea>
-                            </p>
-                        </div>
-                        <div class="botonesTarea">
-                            <button id="marcarCompletada">
-                                <img src="./public/icons/completada.svg">
-                            </button>
-                            <button id="marcarPendiente">
-                                <img src="./public/icons/eliminar.svg">
-                            </button>
-                        </div>
-                    </div>`);
+    
+    function crearTarea() {
+        const nuevaTarea = $(`
+            <div class="tarea" id="tarea-${contadorTareas}">
+                <div class="contenedorTarea">
+                    <p class="tituloTarea">
+                        Título de la tarea:
+                        <input>
+                    </p>
+                    <p class="descripcionTarea">
+                        Descripción de la tarea:
+                        <textarea></textarea>
+                    </p>
+                </div>
+                <div class="botonesTarea">
+                    <button class="marcarCompletada">
+                        <img src="./public/icons/completada.svg">
+                    </button>
+                    <button class="marcarPendiente">
+                        <img src="./public/icons/eliminar.svg">
+                    </button>
+                </div>
+            </div>
+        `);
+        contadorTareas++;
+        return nuevaTarea;
+    }
 
     $("#btnAddTarea").click(function (){
-        contadorTareas++;
-        console.log(contadorTareas);
-        $("#contenedorTareas").prepend(tarea.clone());
-    })
-})
+        const tareaNueva = crearTarea();
+        $("#contenedorTareas").prepend(tareaNueva);
+        console.log("Tarea creada con ID:", tareaNueva.attr('id'));
+    });
+});
